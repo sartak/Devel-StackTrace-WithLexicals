85a55920878f4edfd855de027809df4593ff3e85
Shawn M Moore <sartak@gmail.com>
2009-12-13 07:09:14 -0500

    Devel::StackTrace renamed _ref_as_string to _ref_to_string

--- lib/Devel/StackTrace/WithLexicals.pm
+++ lib/Devel/StackTrace/WithLexicals.pm
@@ -26,7 +26,7 @@ sub _record_caller_data {
         if ($self->{no_refs}) {
             for (values %{ $self->{raw}[$caller]{lexicals} }) {
                 $_ = $$_ if ref($_) eq 'REF';
-                $_ = $self->_ref_as_string($_);
+                $_ = $self->_ref_to_string($_);
             }
         }
     }
@@ -98,7 +98,7 @@ sub _normalize_args {
                 };
             }
             else {
-                $_ = $self->_ref_as_string($_);
+                $_ = $self->_ref_to_string($_);
             }
         }
     }
